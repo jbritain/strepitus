2d434d15998c444bb9c228f1b79c0060986c8289
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-11-15 18:08:36 -0500

    Added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,24 @@ repositories {
     google()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven("https://central.sonatype.com/repository/maven-snapshots/")
+    maven("https://maven.luna5ama.dev")
 }
 
 dependencies {
-}
+    implementation(platform("org.lwjgl:lwjgl-bom:${libs.versions.lwjgl.get()}"))
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = "natives-windows")
 
-kotlin {
-    jvmToolchain(21)
+    implementation(libs.joml)
+
+    implementation(libs.bundles.kotlinEcosystem)
+    implementation(libs.bundles.glWrapper)
+    implementation(libs.bundles.kmogus)
 }
